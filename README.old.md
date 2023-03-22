082cb1d45b1427e93573c8d4673f95200552ecbe
moehzi <faizi1234098@gmail.com>
2023-03-22 18:51:31 +0800

    feat: setting up  project

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# phonebook-miniproject-gt
---
+++ src/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://wpe-hiring.tokopedia.net/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Set any authorization headers or other headers required by the server
+  return {
+    headers: {
+      ...headers,
+      // Add any required headers here, for example:
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
