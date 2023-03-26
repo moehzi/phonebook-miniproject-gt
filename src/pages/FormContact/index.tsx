7d636aca21a77d521c6aebb6a5ae250c52c5ce4b
moehzi <faizi1234098@gmail.com>
2023-03-27 02:01:19 +0800

    feat: input new contact

--- src/pages/FormContact/index.tsx
+++ src/pages/FormContact/index.tsx
@@ -1,16 +1,22 @@
+import { useMutation } from '@apollo/client';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import AlertError from '../../components/AlertError';
 import InputBox from '../../components/InputBox';
 import InputPhone from '../../components/InputPhone';
+import Loader from '../../components/Loader';
+import { ADD_CONTACT_WITH_PHONES } from '../../queries/AddContactWithPhones';
+import { GET_CONTACT_LIST } from '../../queries/GetContactList';
 import { Container } from '../../styles/style';
 import { ButtonCancel, ButtonSubmit } from './style';
 
 const FormContact = () => {
   const [inputData, setInputData] = useState({
-    firstname: '',
-    lastname: '',
+    first_name: '',
+    last_name: '',
     phones: [{ number: '' }],
   });
+  const [errorMsg, setErrorMsg] = useState('');
 
   const navigate = useNavigate();
   const handleCancel = () => {
@@ -24,7 +30,15 @@ const FormContact = () => {
   };
 
   const handleSubmit = () => {
-    console.log(inputData, 'mantap jiwa');
+    if (
+      inputData.first_name &&
+      inputData.last_name &&
+      inputData.phones[0].number
+    ) {
+      addContact({ variables: inputData });
+      return;
+    }
+    setErrorMsg('All fields must be filled!');
   };
 
   const handleAdd = () => {
@@ -47,21 +61,53 @@ const FormContact = () => {
     setInputData(deleteVal);
   };
 
+  const [addContact, { loading }] = useMutation(ADD_CONTACT_WITH_PHONES, {
+    onCompleted: () => {
+      navigate('/');
+    },
+    refetchQueries: [GET_CONTACT_LIST],
+    onError: (error) => {
+      //   setErrorMsg(err.name);
+      console.log(error.graphQLErrors[0].message, 'wadaw');
+      if (error.graphQLErrors[0].message.includes('phone_number_key')) {
+        setErrorMsg(
+          "Phone number is already in the contact, Can't be duplicate!"
+        );
+      }
+    },
+  });
+
+  const handleCloseErr = () => {
+    setErrorMsg('');
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <Container>
+      <h1 className="mb-2 text-2xl font-semibold">Create new contact</h1>
+      {errorMsg && (
+        <AlertError
+          title="Error!"
+          message={errorMsg}
+          onClose={handleCloseErr}
+        />
+      )}
       <InputBox
         onChange={handleChange}
-        id="firstname"
+        id="first_name"
         placeholder="Input your firstname"
         label="Firstname"
-        value={inputData.firstname}
+        value={inputData.first_name}
       />
       <InputBox
         onChange={handleChange}
-        id="lastname"
+        id="last_name"
         placeholder="Input your lastname"
         label="Lastname"
-        value={inputData.lastname}
+        value={inputData.last_name}
       />
       <InputPhone
         state={inputData.phones}
